62811cdccfbfc6f339dfb1847e4ce828ab8c18be
Aginas pk <aginaspk6@gmail.com>
2025-05-12 12:29:02 +0530

    backend

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import mongoose from "mongoose";
+const app = express();
+dotenv.config();
+
+if (!process.env.MONGO_URI) {
+  throw new Error("MONGO_URI is not defined in environment variables");
+}
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("DB Connected"))
+  .catch((err) => console.log(err));
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT;
+app.listen(PORT, () =>
+  console.log(`Server running on http://localhost:${PORT}`)
+);
