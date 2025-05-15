fefedca3fcdea7837782fec6816f3f1ae94700cf
gopika <gopikac680@gmail.com>
2025-05-15 19:23:14 +0530

    api

--- src/Service/ownerService.ts
+++ src/Service/ownerService.ts
@@ -1,9 +1,9 @@
-import Turff, { TurffData } from "../Model/turfModel"
+import Turff, { TurffData } from './../Model/turfModel';
 
 
 export const turfService=async(data:TurffData,filepaths:string[]):Promise<TurffData>=>{
 
-    const{name,city,area,address,turfType,size,image,hourlyRate,status}=data
+    const{name,city,area,address,turfType,size,image,hourlyRate,availability,status}=data
 
     const newTurf=new Turff({
         // ownerId,
@@ -15,8 +15,23 @@ export const turfService=async(data:TurffData,filepaths:string[]):Promise<TurffD
         size,
         image:filepaths,
         hourlyRate,
+        availability,
         status
     })
   const savedTurf=await newTurf.save()
   return savedTurf;
+}
+
+
+export const deleteTurfService=async(id:string)=>{
+
+  return await Turff.findByIdAndUpdate(id,{isDelete:true},{new:true})
+}
+
+
+export const editTurfService=async(id:string,data:TurffData)=>{
+
+  const updateData=await Turff.findByIdAndUpdate(id,{$set:{...data,isDelete:false}},{new:true})
+
+  return updateData;
 }
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import mongoose from "mongoose";
 import ownerRoute from "./Routes/ownerRoute";
-import upload from "./Middleware/uploadMulter";
 const app = express();
 dotenv.config();
 
@@ -19,8 +18,9 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
+//owner apis
+app.use('/api/owner',ownerRoute)
 
-app.use('/api/owner',upload.array('image',5),ownerRoute)
 
 const PORT = process.env.PORT;
 app.listen(PORT, () =>
