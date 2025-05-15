4b3610bab1ee9c1e0d85b43effdf86032961fff0
Aginas pk <aginaspk6@gmail.com>
2025-05-15 22:38:16 +0530

    Admin-user-managment

---
+++ src/Routes/adminRoutes.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import {
+  blockUser,
+  deleteUser,
+  getAllUsers,
+  updateUsers,
+} from "../Controller/adminController";
+const route = express.Router();
+
+route
+  .get("/users", getAllUsers)
+  .put("/update-user/:id", updateUsers)
+  .patch("/block-user/:id", blockUser)
+  .delete("/delete-user/:id", deleteUser);
+
+export default route;
