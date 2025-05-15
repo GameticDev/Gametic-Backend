6dcce3e9eafbd7b73d82bb928dc7c28517181c65
Aginas pk <aginaspk6@gmail.com>
2025-05-15 22:47:38 +0530

    update-admin-user-managment

--- src/Controller/adminController.ts
+++ src/Controller/adminController.ts
@@ -1,7 +1,11 @@
 import { Request, Response } from "express";
 import { asyncErrorhandler } from "../Middleware/asyncErrorHandler";
 import User from "../Model/userModel";
-import { deleteUserService, toggleBlockUser, updateUser } from "../Service/adminServices";
+import {
+  deleteUserService,
+  toggleBlockUser,
+  updateUser,
+} from "../Service/adminServices";
 
 export const getAllUsers = asyncErrorhandler(
   async (req: Request, res: Response) => {
@@ -14,11 +18,8 @@ export const getAllUsers = asyncErrorhandler(
   }
 );
 
-export const updateUsers = async (
-  req: Request,
-  res: Response
-): Promise<void> => {
-  try {
+export const updateUsers = asyncErrorhandler(
+  async (req: Request, res: Response) => {
     const userId = req.params.id;
     const updateData = req.body;
     const updatedUser = await updateUser(userId, updateData);
@@ -38,18 +39,11 @@ export const updateUsers = async (
         isBlocked: updatedUser.isBlocked,
       },
     });
-  } catch (error) {
-    res
-      .status(500)
-      .json({ message: "Server error", error: (error as Error).message });
   }
-};
+);
 
-export const blockUser = async (
-  req: Request,
-  res: Response
-): Promise<void> => {
-  try {
+export const blockUser = asyncErrorhandler(
+  async (req: Request, res: Response) => {
     const userId = req.params.id;
     const updatedUser = await toggleBlockUser(userId);
 
@@ -68,26 +62,19 @@ export const blockUser = async (
         isBlocked: updatedUser.isBlocked,
       },
     });
-  } catch (error) {
-    res
-      .status(500)
-      .json({ message: "Server error", error: (error as Error).message });
   }
-};
+);
 
-
-export const deleteUser = async (req: Request, res: Response): Promise<void> => {
-  try {
+export const deleteUser = asyncErrorhandler(
+  async (req: Request, res: Response) => {
     const userId = req.params.id;
     const deletedUser = await deleteUserService(userId);
-    
+
     if (!deletedUser) {
-      res.status(404).json({ message: 'User not found' });
+      res.status(404).json({ message: "User not found" });
       return;
     }
-    
-    res.status(200).json({ message: 'User deleted successfully' });
-  } catch (error) {
-    res.status(500).json({ message: 'Server error', error: (error as Error).message });
+
+    res.status(200).json({ message: "User deleted successfully" });
   }
-};
+);
