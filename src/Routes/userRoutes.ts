0a4b5c4b79f544508eae26ad7f51f74063e9035b
minhh4j <mnhh4jjjj@gmail.com>
2025-05-16 12:12:32 +0530

    86cyxv647 (#4)
    * loginRegiter
    
    * register
    
    * feat(auth):registration backend
    
    * feat(auth): implement user login functionality with JWT
    
    * feat(auth): implement user login functionality with JWT
    
    ---------
    
    Co-authored-by: GameticDev <gameticdev@gmail.com>

--- src/Routes/userRoutes.ts
+++ src/Routes/userRoutes.ts
@@ -1,9 +1,15 @@
 import express from "express";
+import { loginUser, registerUser } from "../Controller/userController";
+
 import { registerUser } from "../Controller/userController";
+
 
 const router = express.Router();
 
 router.post("/register", registerUser);
+
+router.post('/login' , loginUser)
+
 
 
 export default router;
