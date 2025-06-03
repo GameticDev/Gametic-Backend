55632838b335021539697898ea6a2ced6c2c9aff
Habeeba-Husna <habeebahusna@gmail.com>
2025-06-03 13:15:22 +0530

    feat(owner module backend models changed)

--- src/Routes/ownerRoute.ts
+++ src/Routes/ownerRoute.ts
@@ -1,5 +1,3 @@
-
-
 import express from 'express'
 import { createTurf, deleteTurf, editTurf, getAllturf, turfById  } from '../Controller/ownerController'
 import upload from '../Middleware/uploadMulter';
@@ -7,11 +5,11 @@ import { verifyOwner } from '../Middleware/auth';
 const ownerRoute=express()
 
 
-ownerRoute.post('/addTurf',verifyOwner,upload.array('images',5),createTurf)
-ownerRoute.patch('/editTurf/:id',verifyOwner,upload.array('images',5),editTurf)
-ownerRoute.get('/getAllturf',verifyOwner,getAllturf)
-ownerRoute.delete('/turfs/:id', verifyOwner,deleteTurf)
-ownerRoute.get('/getTurf/:id',verifyOwner,turfById)
+ownerRoute.post('/owner/addTurf',verifyOwner,upload.array('images',5),createTurf)
+ownerRoute.patch('/owner/editTurf/:id',verifyOwner,upload.array('images',5),editTurf)
+ownerRoute.get('/owner/getAllturf',verifyOwner,getAllturf)
+ownerRoute.delete('/owner/turfs/:id', verifyOwner,deleteTurf)
+ownerRoute.get('/owner/getTurf/:id',verifyOwner,turfById)
 
 
 export default ownerRoute;
