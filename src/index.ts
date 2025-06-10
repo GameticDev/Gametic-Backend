0e7854fb4ef441d95ca2e6094e9d341b51052c12
minhh4j <mnhh4jjjj@gmail.com>
2025-06-10 20:03:00 +0530

    cors

--- src/index.ts
+++ src/index.ts
@@ -52,7 +52,7 @@ app.get("/hello", (req, res) => {
 });
 
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () =>
   console.log(`Server running on http://localhost:${PORT}`)
