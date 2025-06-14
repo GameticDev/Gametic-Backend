1b05ffbc2f0afa60f010c11f4e35cf2bc7aa625f
Aginas pk <aginaspk6@gmail.com>
2025-06-14 17:00:49 +0530

    location

---
+++ src/Controller/user/locationController.ts
@@ -0,0 +1,99 @@
+import { NextFunction, Request, Response } from "express";
+import { asyncErrorhandler } from "../../Middleware/asyncErrorHandler";
+import User from "../../Model/userModel";
+import { CustomError } from "../../utils/customError";
+import { Locations } from "../../Model/locationModel";
+interface AuthenticatedRequest extends Request {
+  user?: {
+    userId: string;
+    role: string | undefined;
+  };
+}
+
+interface LocationQuery {
+  search?: string;
+}
+export const updatePreferredLocation = asyncErrorhandler(
+  async (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
+    const { preferredLocation } = req.body;
+    const user = req.user;
+
+    if (!user) {
+      return next(new CustomError("User not authenticated", 401));
+    }
+    console.log(user.role);
+
+    if (user.role !== "user") {
+      return next(
+        new CustomError(
+          "Only users with role 'user' can set preferred location",
+          403
+        )
+      );
+    }
+
+    if (!preferredLocation || typeof preferredLocation !== "string") {
+      return next(
+        new CustomError("Preferred location must be a non-empty string", 400)
+      );
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(
+      user.userId,
+      { preferredLocation },
+      { new: true, runValidators: true }
+    );
+
+    if (!updatedUser) {
+      return next(new CustomError("User not found", 404));
+    }
+
+    res.status(200).json({
+      success: true,
+      data: {
+        _id: updatedUser._id,
+        username: updatedUser.username,
+        email: updatedUser.email,
+        preferredLocation: updatedUser.preferredLocation,
+      },
+    });
+  }
+);
+
+export const getLocations = asyncErrorhandler(
+  async (
+    req: Request<{}, {}, {}, LocationQuery>,
+    res: Response,
+    next: NextFunction
+  ) => {
+    const search = req.query.search as string | undefined;
+
+    const query: Partial<{
+      $or?: Array<{ [key: string]: { $regex: string; $options: string } }>;
+      turfType: string;
+    }> = {};
+    if (search) {
+      query.$or = [{ name: { $regex: search, $options: "i" } }];
+    }
+
+    const totalLocations = await Locations.countDocuments(query);
+
+    const locations = await Locations.find(query)
+      .select("name state")
+      .sort({ name: 1 })
+      .lean();
+
+    if (!locations.length) {
+      return next(new CustomError("No locations found", 404));
+    }
+
+    return res.status(200).json({
+      message: "All locations fetched successfully",
+      locations: locations.map((loc) => ({
+        name: loc.name,
+        state: loc.state,
+      })),
+      totalLocations,
+    });
+  }
+);
--- src/Routes/userRoutes.ts
+++ src/Routes/userRoutes.ts
@@ -35,7 +35,16 @@ import {
 } from "../Controller/user/venueController";
 import upload from "../Middleware/uploadMulter";
 import { authMiddleware } from "../Middleware/auth";
-import { createTournamentPost, getAllTournamentPost, joinTeamToTournament, tournamentById } from "../Controller/tournamentController";
+import {
+  createTournamentPost,
+  getAllTournamentPost,
+  joinTeamToTournament,
+  tournamentById,
+} from "../Controller/tournamentController";
+import {
+  getLocations,
+  updatePreferredLocation,
+} from "../Controller/user/locationController";
 
 const router = express.Router();
 
@@ -69,7 +78,11 @@ router
   .post("/venue-booking", authMiddleware, bookVenue)
   .post("/create-booking-order", authMiddleware, createBookingOrder)
   .get("/getAllVenues", getAllVenuesforUser)
-  .get("/veunesById/:turfId", getVenueByIdforUser);
+  .get("/veunesById/:turfId", getVenueByIdforUser)
+
+  //location
+  .patch("/update-location", authMiddleware, updatePreferredLocation)
+  .get("/getLocations", getLocations);
 
 router.post("/addMatch", addPost);
 router.get("/getAllPost", getAllPost);
@@ -84,7 +97,6 @@ router.post("/team", createTeam);
 
 router.post("/updateprofile", upload.single("picture"), updateUser);
 
-
 router.post("/check", loginUser);
 router.get("/postById/:id", getPostById);
 
@@ -97,15 +109,19 @@ router.post("/team", createTeam);
 
 router.post("/check", loginUser);
 
+router.get("/getAllTournament", getAllTournamentPost);
+router.post(
+  "/createTournament",
+  authMiddleware,
+  upload.single("image"),
+  createTournamentPost
+);
 
-router.get('/getAllTournament',getAllTournamentPost)
-router.post('/createTournament',authMiddleware,upload.single('image'),createTournamentPost)
+router.post("/team", authMiddleware, createTeam);
 
-router.post('/team',authMiddleware,createTeam)
+router.get("/tournamentById/:id", tournamentById);
 
-router.get('/tournamentById/:id',tournamentById)
-
-router.patch('/tournament/:id/join-team',joinTeamToTournament)
-router.post("/user", authMiddleware ,LoginedUserDetails);
+router.patch("/tournament/:id/join-team", joinTeamToTournament);
+router.post("/user", authMiddleware, LoginedUserDetails);
 
 export default router;
